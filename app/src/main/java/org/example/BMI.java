034068e49c32d8f959dd0aa0803b66a363401a49
bonguen <burakong44@outlook.de>
2024-10-24 11:43:49 +0200

    implement rounding functionality

--- app/src/main/java/org/example/BMI.java
+++ app/src/main/java/org/example/BMI.java
@@ -3,6 +3,7 @@ package org.example;
 public class BMI {
 
     public double calculateBMI(double height, double weight) {
-        return weight / (height * height);
+        double bmi = weight / (height * height);
+        return Math.round(bmi * 10.0) / 10.0;
     }
 }
