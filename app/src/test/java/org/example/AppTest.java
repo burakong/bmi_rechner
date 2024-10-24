26795046a7ba26a23681427aa448eeebc80a65d8
bonguen <burakong44@outlook.de>
2024-10-24 11:52:16 +0200

    add two new test cases, which one are testing the functionality of the bmi calculator

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -3,8 +3,26 @@
  */
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+    @Test
+    @DisplayName("should display bmi after adding weigth and height of person one")
+    void testBmiCalculationPerson1() {
+        BMI bmiCalculator = new BMI();
+        double person1Bmi = bmiCalculator.calculateBMI(1.75, 70);
+        double expected = 22.9;
+        assertEquals(expected, person1Bmi);
+    }
+
+    @Test
+    @DisplayName("should display bmi after adding weigth and height of a person two")
+    void testBmiCalculationPerson2() {
+        BMI bmiCalculator = new BMI();
+        double person1Bmi = bmiCalculator.calculateBMI(1.80, 85);
+        double expected = 26.2;
+        assertEquals(expected, person1Bmi);
+    }
 }
